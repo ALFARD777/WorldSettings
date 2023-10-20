3f1de14e136b338fb7659dcbaea0f1fdca0eb22b
ALFARD777 <alfard6066@gmail.com>
2023-10-21 00:38:14 +0300

    database update

--- src/main/java/org/desparodev/worldsettings/WorldSettings.java
+++ src/main/java/org/desparodev/worldsettings/WorldSettings.java
@@ -1,11 +1,21 @@
 package org.desparodev.worldsettings;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 import java.util.Objects;
 import java.util.logging.Level;
 
 public final class WorldSettings extends JavaPlugin {
     @Override
     public void onEnable() {
+        File configFile = new File(getDataFolder(), "config.ini");
+        if (!configFile.exists()) {
+            getConfig().options().copyDefaults(true);
+            saveConfig();
+        }
+        MySqlDataBase db = new MySqlDataBase(getConfig().getString("mysql.host"), getConfig().getInt("mysql.port"), getConfig().getString("mysql.database"), getConfig().getString("mysql.username"), getConfig().getString("mysql.password"));
+
+
         SettingsCommand sc = new SettingsCommand();
         Objects.requireNonNull(getCommand("settings")).setExecutor(sc);
         Objects.requireNonNull(getCommand("reloadSB")).setExecutor(sc);
@@ -17,3 +27,4 @@ public final class WorldSettings extends JavaPlugin {
         getLogger().log(Level.INFO, "Отключен!");
     }
 }
+
